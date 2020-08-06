e0213cd7e02acc061d49a60dad64eb0384054c1a
forgreatness <danh1nguyen23@live.com>
2020-08-06 00:00:42 -0700

    because I am done with all the features for this project

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ import FilmsData from './data/films.json';
 
 function Home() {
   return (
-    <h2>Welcome to my React Router App</h2>
+    <div id="home-content">
+    </div>
   );
 }
 
@@ -49,7 +50,15 @@ function People() {
       </div>
       <div class="main">
         <Switch>
-          {peopleRoutesMatcher}
+          {[
+            ...peopleRoutesMatcher,
+            <Route exact path={url}>
+              <p>Select one of the character on the side navigation to learn more about their background.</p>
+            </Route>,
+            <Route path="*">
+              <div>404 - Page Not Found</div>
+            </Route>
+          ]}
         </Switch>
       </div>
     </React.Fragment>
@@ -59,6 +68,12 @@ function People() {
 function Person(props) {
   const person = props.info;
   const filmsPresent = [];
+
+  person.films.forEach(film => {
+    filmsPresent.push(
+      <li><Link to={film}>{film}</Link></li>
+    );
+  });
 
   return (
     <React.Fragment>
@@ -70,10 +85,10 @@ function Person(props) {
       <span><b>Eye Color:  </b>{person.eye_color}</span> <br />
       <span><b>Birth Year: </b>{person.birth_year}</span> <br />
       <span><b>Gender: </b>{person.gender}</span> <br />
-      <span><b>Home World: </b>{person.homeworld}</span> <br />
+      <span><b>Home World: </b><Link to={person.homeworld}>{person.homeworld}</Link></span> <br />
       <b>Films:</b>
       <ul>
-
+        {filmsPresent}
       </ul>
     </React.Fragment>
   );
@@ -83,12 +98,19 @@ function Person(props) {
 function Planets() {
   const { url, path } = useRouteMatch();
   const planetLinks = [];
+  const planetRoutesMatcher = [];
 
   for (var planet of Object.keys(PlanetsData)) {
     planetLinks.push(
       <li key={planet}>
         <NavLink to={PlanetsData[planet].url}>{PlanetsData[planet].name}</NavLink>
       </li>
+    )
+
+    planetRoutesMatcher.push(
+      <Route path={PlanetsData[planet].url}>
+        <Planet info={PlanetsData[planet]} />
+      </Route>
     )
   }
 
@@ -100,8 +122,66 @@ function Planets() {
         </ul>
       </div>
       <div class="main">
-
+        <Switch>
+          {
+            [
+              ...planetRoutesMatcher,
+              <Route exact path={url}>
+                <p>Select a planet from the side navigation to explore its location.</p>
+              </Route>,
+              <Route path="*">
+                <div>
+                  404 - Page Not Found
+                </div>
+              </Route>
+            ]
+          }
+        </Switch>
       </div>
+    </React.Fragment>
+  );
+}
+
+function Planet(props) {
+  const planet = props.info;
+  const planetResidents = [];
+  const filmsPresent = [];
+
+  planet.residents.forEach(resident => {
+    planetResidents.push(
+      <li>
+        <Link to={resident}>{resident}</Link>
+      </li>
+    );
+  });
+
+  planet.films.forEach(film => {
+    filmsPresent.push(
+      <li>
+        <Link to={film}>{film}</Link>
+      </li>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <h2>{planet.name}</h2>
+      <span><b>Rotation Period: </b>{planet.rotation_period}</span> <br />
+      <span><b>Orbital Period: </b>{planet.orbital_period}</span> <br />
+      <span><b>Diameter: </b>{planet.diameter}</span> <br />
+      <span><b>Climate: </b>{planet.climate}</span> <br />
+      <span><b>Gravity: </b>{planet.gravity}</span> <br />
+      <span><b>Terrain: </b>{planet.terrain}</span> <br />
+      <span><b>Surface Water: </b>{planet.surface_water}</span> <br />
+      <span><b>Population: </b>{planet.population}</span> <br />
+      <b>Residents: </b>
+      <ul>
+        {planetResidents}
+      </ul>
+      <b>Films: </b>
+      <ul>
+        {filmsPresent}
+      </ul>
     </React.Fragment>
   );
 }
@@ -109,6 +189,7 @@ function Planets() {
 function Films() {
   const { url, path } = useRouteMatch();
   const filmLinks = [];
+  const filmRoutesMatcher = [];
 
   for (var film of Object.keys(FilmsData)) {
     filmLinks.push(
@@ -116,6 +197,12 @@ function Films() {
         <NavLink to={FilmsData[film].url}>{FilmsData[film].title}</NavLink>
       </li>
     )
+
+    filmRoutesMatcher.push(
+      <Route path={FilmsData[film].url}>
+        <Film info={FilmsData[film]} />
+      </Route>
+    );
   }
 
   return (
@@ -126,8 +213,63 @@ function Films() {
         </ul>
       </div>
       <div class="main">
-
+        <Switch>
+          {
+            [
+              ...filmRoutesMatcher,
+              <Route exact path={url}>
+                <p>Select a film from the side navigation to learn about its details.</p>
+              </Route>,
+              <Route path="*">
+                <div>
+                  404 - Page Not Found
+                </div>
+              </Route>
+            ]
+          }
+        </Switch>
       </div>
+    </React.Fragment>
+  );
+}
+
+function Film(props) {
+  const film = props.info;
+  const people = [];
+  const planets = [];
+
+  film.characters.forEach(character => {
+    people.push(
+      <li>
+        <Link to={character}>{character}</Link>
+      </li>
+    );
+  });
+
+  film.planets.forEach(planet => {
+    planets.push(
+      <li>
+        <Link to={planet}>{planet}</Link>
+      </li>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <h2>{film.title}</h2>
+      <span><b>Episode ID: </b>{film.episode_id}</span> <br />
+      <span><b>Opening Crawl: </b>{film.opening_crawl}</span> <br />
+      <span><b>Director: </b>{film.director}</span> <br />
+      <span><b>Producer: </b>{film.producer}</span> <br />
+      <span><b>Release Date: </b>{film.release_date}</span> <br />
+      <b>Characters: </b>
+      <ul>
+        {people}
+      </ul>
+      <b>Planets: </b>
+      <ul>
+        {planets}
+      </ul>
     </React.Fragment>
   );
 }
